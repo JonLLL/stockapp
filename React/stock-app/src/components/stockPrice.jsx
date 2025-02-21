e7a82bf1cea2eda19bf3d577253f86e0e1739e87
Jonathan Luo <jl1528@rit.edu>
2025-02-20 23:32:46 -0500

    ui updates

--- React/stock-app/src/components/stockPrice.jsx
+++ React/stock-app/src/components/stockPrice.jsx
@@ -8,6 +8,9 @@ function StockPrice(){
     const[prices, setPrices] = useState([]);
     const [symbol, setSymbol] = useState("");
     const [exchange, setExchange] = useState("");
+    const [page, setPage] = useState(0);
+    const pageSize = 30;
+    
     
     useEffect(() => {
         const fetchPrices = async () => {
@@ -22,6 +25,10 @@ function StockPrice(){
         };
         fetchPrices();
     }, [assetId]);
+    const totalPages = Math.ceil(prices.length / pageSize);
+    const startIndex = page * pageSize;
+    const endIndex = startIndex + pageSize;
+    const visibleData = prices.slice(startIndex, endIndex);
 
     return (
         <div>
@@ -29,18 +36,52 @@ function StockPrice(){
             <div style={{ width: '100%', height: '500px' }}>
               <TradingViewWidget symbol = {symbol} exchange = {exchange}/>
             </div>
-          <h2>Price History</h2>
+          <h4>Price History - From :1/2/2024  To: 11/30/2024</h4>
           {prices.length === 0 ? (
             <p>Prices Currently not available</p>
           ):(
-          <ul>
-            {prices.map((price, index) => (
-              <li key={index}>
-                {price.time_stamp} - open: {price.open}, close: {price.close}, high: {price.high}, low: {price.low} , volume:{price.volume}
-              </li>
-            ))}
-          </ul>
-          )}
+              <>
+              <div>
+                  <button
+                    onClick={() => setPage((prev) => Math.max(prev - 1, 0))}
+                    disabled={page === 0}
+                  >
+                    ◀ 
+                  </button>
+                  <span> Page {page + 1} of {totalPages} </span>
+                  <button
+                    onClick={() => setPage((prev) => Math.min(prev + 1, totalPages - 1))}
+                    disabled={page >= totalPages - 1}
+                  >
+                     ▶
+                  </button>
+                </div>
+                  <table border="1" style={{ width: "100%", textAlign: "center", borderCollapse: "collapse" }}>
+                    <thead>
+                      <tr>
+                        <th>Time</th>
+                        <th>Open</th>
+                        <th>Close</th>
+                        <th>High</th>
+                        <th>Low</th>
+                        <th>Volume</th>
+                      </tr>
+                    </thead>
+                      <tbody>
+                        {visibleData.map((price, index) => (
+                          <tr key={index}>
+                            <td>{price.time_stamp}</td>
+                            <td>{price.open}</td>
+                            <td>{price.close}</td>
+                            <td>{price.high}</td>
+                            <td>{price.low}</td>
+                            <td>{price.volume}</td>
+                          </tr>
+                        ))}
+                      </tbody>
+                </table>
+              </>
+            )}
           
         </div>
       );
