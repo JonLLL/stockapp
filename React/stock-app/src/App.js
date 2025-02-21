0d827e257d2390eaee437fcd25aefc6b86cf8568
Jonathan Luo <jl1528@rit.edu>
2025-02-21 13:11:16 -0500

    ui updates

--- React/stock-app/src/App.js
+++ React/stock-app/src/App.js
@@ -9,14 +9,14 @@ const App = () => {
   return(
     <Router>
       <div style={{ fontFamily: 'Arial, sans-serif', padding: '20px' }}>
-        <h1>Stock Application</h1>
-
+        <h1>
+          <Link to="/" style={{ textDecoration: 'none', color: 'inherit' }}>
+            AssetLab
+          </Link>
+        </h1>
         {/* Navigation links */}
         <nav>
           <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
             <li>
               <Link to="/assets">Asset List</Link>
             </li>
@@ -34,8 +34,9 @@ const App = () => {
             element={
               <div>
                 <h2>Welcome to my stock app project</h2>
-                <h3>Seamlessly manage your stocks, explore historical price data, and use detailed charts from tradeview to guide your financial journey!</h3>
-                <h4>Discover and explore a wide range of assets with ease. Start searching below!</h4>
+                <h3>Seamlessly manage your stocks, explore historical price data, 
+                  and use detailed charts from tradeview to guide your financial journey!</h3>
+                <h4>Discover and explore through more than 11,000 stocks and other assets. Start searching below!</h4>
                 <SearchDropdown />
               </div>
             }
@@ -50,8 +51,6 @@ const App = () => {
           {/* Login Route */}
           <Route path="/login" element={<Login />} />
         </Routes>
-
-        {/* Button to navigate to login page */}
       </div>
     </Router>
   );
